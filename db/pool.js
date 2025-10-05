bb4c29bceb1d075b852d55c8c22a6e999ef276d7
Elanchezhiyan <elanchezhiyan2440@gmail.com>
2025-10-06 00:25:27 +0530

    added internal URL for render

--- db/pool.js
+++ db/pool.js
@@ -2,7 +2,7 @@ const {Pool} = require('pg')
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString:process.env.DB_URL,
+    connectionString:process.env.DB_URL || 'postgresql://wrath:poorni@localhost:5432/inventorypostgresql://wrath:LtoJcjjMllTefsBtesz0Ctu1vkNL4cXJ@dpg-d3h9b049c44c739olf90-a/inventory_y6a6',
 })
 
 module.exports = pool
